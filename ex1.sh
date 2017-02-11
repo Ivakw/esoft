11735d5cdb9be38f559c0aa862208fd00694725a
piusha <piushakalyana@gmail.com>
2017-02-11 07:57:48 +0000

    Baisc shel

---
+++ ex1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+clear
+echo "current date :- " `date` 
+
+echo "Current User :-" `who | cut -d " " -f1`;
+
+echo "Current Dir";
+pwd
+
+echo "asdkasjdl jasd ajsdh ahsdjashljdhals djhasl dhasjd \njhasjdkahjksdhkajshd klasas\njahsd kahjsld ahjsd";
+
+
