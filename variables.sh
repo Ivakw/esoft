44a3a61e563537c846a2191e90e14452faaed541
piusha <piushakalyana@gmail.com>
2017-02-18 04:13:17 +0000

    Variables shell_scripts

---
+++ variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "Emp ID"
+read empId;
+echo "Enter First Name ";
+read fName;
+echo "Enter Last Name ";
+read lName;
+echo "Enter DOB";
+read dob;
+
+
+echo -e "\n\n"
+
+echo "===============EMPLOYES======================";
+echo -e "\n\n"
+echo "Employe ID :- "$empId; 
+echo "Employe Nme:-" $fName" "$lName;
+echo "Date of birth :-" $dob;
+echo ""
+
